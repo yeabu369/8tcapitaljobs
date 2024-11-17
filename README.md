9eca932a8d852ffbd607f3dfc0cd2d3de6a14ad8
yeabu369 <40659499+yeabu369@users.noreply.github.com>
2024-11-17 00:30:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next.js 14 Job Board
+
+It shows how to use server actions from server components, from client components, with and without React Hook Form, and how to achieve **progressive enhancement** with the `useFormStatus` & `useFormState` hooks.
---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "8T Jobs",
+    template: "%s | 8T Jobs",
+  },
+  description: "Find your dream developer job.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} min-w-[350px]`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
